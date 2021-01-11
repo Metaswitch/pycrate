326d2103c4cc03ab80733d38224d0ff6fb3fdf1b
cns <christopher.swindle@metaswitch.com>
2021-01-11 09:45:02 +0000

    Update version to 0.4.1

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="pycrate",
-    version="0.4",
-    
+    version="0.4.1",
+
     #packages=find_packages(),
     packages=["pycrate_core",
               "pycrate_ether",
@@ -21,32 +21,32 @@ setup(
               "pycrate_crypto",
               ],
     test_suite="test.test_pycrate",
-    
+
     scripts=["tools/pycrate_asn1compile.py",
              "tools/pycrate_berdecode.py",
              "tools/pycrate_showmedia.py",
              "tools/pycrate_map_op_info.py",
              ],
-    
+
     # no mandatory dependency
     install_requires=[],
-    
+
     # optional dependencies
     extras_require={
         "NASLTE"  : ["CryptoMobile"],
         "corenet" : ["pysctp", "CryptoMobile"],
         "diameter_dict" : ["lxml"],
         },
-    
+
     # for pycrate_asn1dir and pycrate_csn1dir:
     # .asn, .csn, .txt and .json files from asn1dir and csn1dir are not required
-    # to be installed 
+    # to be installed
     # only compiled .py modules are installed by default
     # for pycrate_diameter:
     # .xml files are required as they are transformed into Python dict when the
     # module is loaded
     # for pycrate_corenet:
-    # the AuC.db file is the mobile subscriber authentication database 
+    # the AuC.db file is the mobile subscriber authentication database
     # containing Ki and is required at runtime when using corenet
     package_data={
         #"pycrate_asn1dir"  : ["*.asn", "*.json"],
@@ -54,12 +54,12 @@ setup(
         "pycrate_diameter" : ["*.xml"],
         },
     #include_package_data=False,
-    
+
     author="Benoit Michau",
     author_email="michau.benoit@gmail.com",
     description="A software suite to handle various data formats",
     long_description=open("README.md", "r").read(),
-    url="https://github.com/P1sec/pycrate/",
+    url="https://github.com/Metaswitch/pycrate/",
     keywords="protocol format asn1 compiler csn1 encoder decoder mobile core network Diameter NAS S1AP TCAP MAP",
     license="LGPL v2.1+",
     )
